30ca11ccc7d2dbe8a1e3f753365eec22527776e8
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-09-15 09:56:42 +0530

    login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -16,36 +16,48 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDcV6gVxuDuFuJJjOihcaP9NS4gtTGFWmo";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDcV6gVxuDuFuJJjOihcaP9NS4gtTGFWmo",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = "Authentication  failed";
-            // if (data && data.error && data.error.message) {
-            //   errorMessage = data.error.message;
-            // }
-            alert(errorMessage);
-          });
-        }
-      });
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDcV6gVxuDuFuJJjOihcaP9NS4gtTGFWmo";
+     
     }
+    fetch(
+      url,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Authentication  failed";
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          alert(errorMessage);
+          throw new Error (errorMessage);
+        });
+      }
+    }).then ((data)=>{
+      console.log(data)
+    }).catch((err)=>{
+      alert(err.message)
+    })
   };
 
   return (
